a85e88e2ce424a947661081c02adb350d09ae6e8
Aleksei Smelianski <alekseismelianski@alekseis-mbp.fios-router.home>
2018-03-24 10:47:09 -0400

    fix on line 19 where if let statement was coded wrong

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -16,7 +16,7 @@ class TodoListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = itemArray = defaults.array(forKey: "TodoListArray") as! [String] {
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
             itemArray = items
         }
         
